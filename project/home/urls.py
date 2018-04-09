323ed52acb813f62033400108e9b0964b9f3f9e6
Alex St <xelaxela13@gmail.com>
2018-04-09 21:26:24 +0300

    Bootstrap4 base template

--- project/home/urls.py
+++ project/home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import Index
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('', Index.as_view(), name='home'),
+    path('', login_required(Index.as_view()), name='home'),
 ]
--- project/home/views.py
+++ project/home/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.contrib.auth.decorators import login_required
 
 
 class Index(TemplateView):
     template_name = 'home/index.html'
 
-    def dispatch(self, request, *args, **kwargs):
-        return super(Index, self).dispatch(request, *args, **kwargs)
